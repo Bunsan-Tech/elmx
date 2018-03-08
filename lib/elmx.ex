abe8997f667acda159294841acf10832889f56e6
silmood <dsilmood@gmail.com>
2018-03-07 19:20:33 -0600

    🚀 Template project is working

--- lib/elmx.ex
+++ lib/elmx.ex
@@ -1,18 +1,2 @@
 defmodule Elmx do
-  @moduledoc """
-  Documentation for Elmx.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elmx.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- lib/mix/tasks/elmx.ex
+++ lib/mix/tasks/elmx.ex
@@ -4,54 +4,26 @@ defmodule Mix.Tasks.Elmx.New do
   def run([name]) do
     Mix.Task.run("phx.new", [name])
 
-    Mix.shell.info [:green, "* creating ", :default_color, name <> "/assets/elm"]
+    Mix.shell.info [:green, "* running ", :default_color, "create-elm-app"]
     System.cmd "create-elm-app", ["elm"], cd: name <> "/assets"
-    System.cmd "npm", ["install","--save-dev", "elm-brunch"], cd: name <> "/assets"
+    System.cmd "npm", ["install","--save-dev", "elm-brunch"], [cd: name <> "/assets", stderr_to_stdout: true]
+    configure_elm(name)
 
-    Mix.shell.info [:green, "* configuring ", :default_color, "elm-brunch"]
-
-    File.stream!(name <> "/assets/brunch-config.js")
-    |> Stream.map(fn line ->
-      cond do
-        String.contains? line, "plugins:" -> elm_brunch_config()
-        String.contains? line, "ignore:" -> babel_brunch_config()
-        true -> line
-      end
-    end)
-    |> Stream.into(File.stream!(name <> "/assets/brunch-config-2.js", [:write, :utf8]))
-    |> Stream.run
-
-    Mix.shell.info [:default_color, "elm was configured"]
+    Mix.shell.info [:cyan, "\nElm was configured successfully!\n"]
   end
 
-  defp babel_brunch_config do
-    [{3, "ignore: ["}, 
-    {4, ~s(/vendor/,)}, 
-    {4, ~s("js/elm.js"]\n)}]
-    |> indent_lines()
+  defp configure_elm(project_name) do
+    Mix.shell.info [:green, "* configuring ", :default_color, "elm app"]
+    File.cp! brunch_config(), project_name <> "/assets/brunch-config.js"
+    File.cp! app_js(), project_name <> "/assets/js/app.js"
+    File.cp! index(), project_name <> "/lib/" <> project_name <> "_web/templates/page/index.html.eex"
+    File.cp! main(), project_name <> "/assets/elm/src/Main.elm"
+    System.cmd "mv", [ project_name <> "/assets/elm/public/logo.svg", project_name <> "/assets/static/images/logo.svg"]
   end
 
-  defp elm_brunch_config do
-    [{1, ~s(plugins: {)}, 
-    {2, "elmBrunch: {" }, 
-    {3, ~s(elmFolder: "elm",)},
-    {3, ~s(mainModules: ["src/Main.elm"],)},
-    {3, ~s(outputFolder: "../js",)}, 
-    {3, ~s(outputFile: "elm.js",)} , 
-    {3, ~s(makeParameters: ["--warn"])},
-    {2, "},\n"}] 
-    |> indent_lines()
-  end
-
-  defp indent_lines(lines) do
-    Enum.map_join(lines, "\n", fn(line) ->
-      indent_line(line)
-    end)
-  end
-
-  defp indent_line({levels, line}) do
-    String.duplicate("  ", levels) <> line
-  end
-
+  defp brunch_config, do: Path.expand "../../../priv/brunch-config.js", __DIR__
+  defp app_js, do: Path.expand "../../../priv/app.js", __DIR__ 
+  defp index, do: Path.expand "../../../priv/index.html.eex", __DIR__ 
+  defp main, do: Path.expand "../../../priv/Main.elm", __DIR__ 
 
 end
